04f9b8c3f9a1d603327803765fba5875b0220154
Jaden <jaden.stanton6@gmail.com>
2024-03-13 21:56:50 -0500

    Setup essential React tools and libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import components
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to React
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <p>Welcome to Share It</p>
+        </header>
+        {/* <Routes>
+          <Route path='/about' element={<About />} />
+        </Routes> */}
+      </div>
+    </Router>
   );
 }
 
